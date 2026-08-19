cabd6027848796d8555fe77c8d4b6214d09e0ada
GuavaRose <36986473+GuavaRose@users.noreply.github.com>
2026-08-19 13:34:04 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -783,13 +783,11 @@ const App = () => {
                     <div className="absolute top-[120px] bottom-[100px] left-[80px] right-[80px] z-30 flex flex-col bg-transparent px-[80px] py-[60px]">
                       <div className="flex-1 flex flex-col items-center justify-center mt-[40px]">
                         <h1
-                          className="font-bold text-gray-900 leading-[1.3] text-center whitespace-pre-wrap mb-[20px]"
+                          className="font-bold text-gray-900 leading-[1.3] text-center whitespace-pre-wrap w-full mb-[60px]"
                           style={{
                             ...serifFont,
                             fontSize: `${topicSize}px`,
-                            wordBreak: 'break-word',
-                            width: 'calc(100% + 80px)',
-                            marginLeft: '-40px'
+                            wordBreak: "break-word",
                           }}
                         >
                           {texts.topic}
